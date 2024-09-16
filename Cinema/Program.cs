98b2581cd397189dfc59d67f13a8d4123c5c159b
Alexander Gustafsson <alegus753@gmail.com>
2024-09-16 13:11:54 +0200

    Error handling and comments

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Cinema
@@ -11,27 +12,34 @@ namespace Cinema
         static void Main(string[] args)
         {
             bool openProgram = true;
-
             Visitors visitor = new Visitors();
 
             while (openProgram)
             {
-                Console.WriteLine("HUVUDMENY BIO - Skriv en siffra nedan och tryck enter" +
-                                  "\n1. En person" +
-                                  "\n2. Flera personer" +
-                                  "\n3. Loop" +
-                                  "\n4. Split" +
-                                  "\n5. Stäng programmet");
+                Console.WriteLine("HUVUDMENY - Skriv en siffra nedan och tryck enter för att testa en funktion" +
+                                  "\n1. Bio, en person" +
+                                  "\n2. Bio, flera personer" +
+                                  "\n3. Upprepa inmatad text 10 gånger" +
+                                  "\n4. Hitta tredje ordet i en mening" +
+                                  "\n0. Stäng programmet");
                 string input = Console.ReadLine();
 
-                int.TryParse(input, out int menuNumber);
+                // check if the user input is indeed a number
+                bool menuSuccess = int.TryParse(input, out int menuNumber);
+                if (menuSuccess == false)
+                    menuNumber = -1;
 
                 switch (menuNumber)
                 {
+                    // simply close the program
+                    case 0:
+                        openProgram = false;
+                        break;
+                    // check the age of one cinema-goer, return a string depending on the age
                     case 1:
                         Console.Write("\nSkriv din ålder: ");
-                        bool success = int.TryParse(Console.ReadLine(), out int age);
-                        if (success)
+                        bool ageSuccess = int.TryParse(Console.ReadLine(), out int age);
+                        if (ageSuccess)
                         {
                             if (age < 0)
                             {
@@ -55,24 +63,39 @@ namespace Cinema
                             Console.Write("\nFelaktik inmatning, prova igen." + "\n");
                         }
 
-                        //payroll.AddEmployee(employeeName, employeSalary);
                         break;
+                    // handle muliple cinema-goers, calculate the number of people and the total cost
                     case 2:
                         Console.Write("\nSkriv antalet besökare: ");
                         int.TryParse(Console.ReadLine(), out int visitorInput);
 
+                        // make sure that the user input is a number that is not negative, otherwise break the program
+                        bool multipleAgeSuccess = true;
                         for (int i = 0; i < visitorInput; i++)
                         {
                             Console.Write($"Skriv åldern för besökare nummer {i + 1}: ");
-                            int.TryParse(Console.ReadLine(), out age);
+                            multipleAgeSuccess = int.TryParse(Console.ReadLine(), out age);
+
+                            if (multipleAgeSuccess == false || age < 0)
+                            {
+                                Console.Write("\nFelaktik inmatning, prova igen." + "\n");
+                                break;
+                            }
+                            // store the visitors and their age
                             visitor.AddVisitor(age);
                         }
-                        
+
+                        if (multipleAgeSuccess == false)
+                        {
+                            break;
+                        }
+                        // call a function to get all the visitors ages
                         IEnumerable<Age> visitors = visitor.GetVisitors();
 
                         int people = 0;
                         int total = 0;
 
+                        // calculate the cost of the group
                         foreach (var singleVisitor in visitors)
                         {
                             if (singleVisitor.VisitorAge < 20)
@@ -90,8 +113,9 @@ namespace Cinema
                             people++;
                         }
 
-                        Console.WriteLine($"\nAntal personer: {people}st" + $"\nTotal kostnad: {total}kr");
+                        Console.WriteLine($"\nAntal personer: {people} st" + $"\nTotal kostnad: {total}kr");
                         break;
+                    // repeat a sentence 10 times
                     case 3:
                         Console.Write("\nSkriv en mening som kommer upprepas: ");
                         string sentence = Console.ReadLine();
@@ -105,29 +129,29 @@ namespace Cinema
                         }
 
                         break;
+                    // take in a sentence and then find the thirs word via the .Split function
                     case 4:
                         Console.Write("\nSkriv en mening med minst tre ord: ");
                         var solidInput = Console.ReadLine();
+                        // pattern that removes multiple spaces
+                        string pattern = @"\s+";
+                        var splitInput = Regex.Split(solidInput, pattern);
 
-                        string[] splitInput = solidInput.Split(' ');
-
-                        if (splitInput.Length < 3)
+                        // check to see if the user input is correct
+                        if (splitInput.Length < 3 || splitInput[0].Length < 1 || splitInput[1].Length < 1 || splitInput[2].Length < 1)
                         {
-                            Console.Write("\nFelaktik inmatning, prova igen." + "\n");
+                            Console.Write("\nFelaktik inmatning, se till att meningen inehåller minst 3 ord." + "\n");
                             break;
                         }
 
                         Console.WriteLine($"Det tredje ordet i din mening är: {splitInput[2]}");
 
                         break;
-                    case 5:
-                        openProgram = false;
-                        break;
                     default:
                         Console.WriteLine("\nFelaktik inmatning, prova igen.");
                         break;
                 }
-
+                // just some neet styling for the program
                 Console.WriteLine("\n==========================" + "\n");
             }
         }
